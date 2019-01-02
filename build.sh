44f905e1169d47696818b557eb88e06644e02bac
brinkqiang <brink.qiang@gmail.com>
2019-01-02 21:26:05 +0800

    ad

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# - install depends tools
+# yum -y install git
+# yum -y install gcc gcc-c++ autoconf libtool automake make
+#
+
+# - clone code
+# git clone https://github.com/brinkqiang/htop.git
+# pushd htop
+# git submodule update --init --recursive
+#
+
+libtoolize && aclocal && autoheader && autoconf && automake --add-missing
+sh configure && make
+
+# popd
+
+# echo continue && read -n 1
